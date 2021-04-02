ac2b1b3868c119e3d63895266ae73450712ec47f
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-02 11:39:45 +0530

    Added functionality to know the MIME type of files and on the basis of that play MP3 files

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const electron = require("electron");
 const fs = require("fs");
 const path = require("path");
 const dataurl = require("dataurl");
+const FileType = require("file-type");
 const mp3Duration = require("mp3-duration");
 const NodeID3 = require("node-id3");
 const app = electron.app;
@@ -70,14 +71,15 @@ ipcMain.on("modal-file-content", (event, arg) => {
 const getDuration = (filePath) => {
   const durationPromise = new Promise((resolve, reject) => {
     mp3Duration(filePath, (err, duration) => {
-      if (duration) {
-        resolve(duration);
-      } else if (!duration) {
-        console.log("Duration mp3 to img error");
-        reject("Fuck");
-      }
       if (err) {
-        reject(err);
+        console.log("ERROR! Reading File in getDuration");
+        return reject(err);
+      }
+      if (duration) {
+        return resolve(duration);
+      } else {
+        console.log("ERROR! Duration property is not present in file");
+        return reject("ERROR! Duration property is not present in file");
       }
     });
   });
@@ -89,20 +91,22 @@ const getTags = (track) => {
   const tagsPromise = new Promise((resolve, reject) => {
     NodeID3.read(filePath, function (err, tags) {
       if (err) {
-        reject(err);
+        console.log("ERROR! Reading File in getTags");
+        return reject(err);
       }
       if (tags) {
-        // console.log(tags);
-        // console.log(tags.image.imageBuffer);
-        const imageBuffer = Buffer.from(tags.image.imageBuffer).toString(
-          "base64"
-        );
+        let imageBuffer;
+
+        if (tags.image && tags.image.imageBuffer) {
+          imageBuffer = Buffer.from(tags.image.imageBuffer).toString("base64");
+        }
+
         const { title, album, artist } = tags;
         Object.assign(track, { title, artist, album, imageBuffer });
-        resolve(track);
+        return resolve(track);
       } else {
-        console.log("When image file converted to mp3 error");
-        reject("Shit");
+        console.log("ERROR! Tags property not present in file");
+        return reject("ERROR! Tags property not present in file");
       }
     });
   });
@@ -110,7 +114,6 @@ const getTags = (track) => {
 };
 
 const createSongObject = (data) => {
-  // let songObjectArray = [];
   let promises = [];
   data.forEach((file) => {
     promises.push(
@@ -123,43 +126,40 @@ const createSongObject = (data) => {
             return getTags(data);
           })
           .then((data) => resolve(data))
-          .catch((err) => reject("FUCK"));
+          .catch((err) => reject(err));
       })
     );
   });
-  return Promise.all(promises);
-  // const songObjectPromise = new Promise((resolve, reject) => {
-  //   data.forEach((file) => {
-  //     getDuration(file.filePath)
-  //       .then((duration) => {
-  //         return Object.assign(file, { duration });
-  //       })
-  //       .then((data) => {
-  //         return getTags(data);
-  //       })
-  //       .then((finalMusic) => {
-  //         songObjectArray.push(finalMusic);
-  //       });
-  //   });
-  //   resolve(songObjectArray);
-  // });
 
-  // return songObjectPromise;
-  // return getDuration(data.filePath)
-  //   .then((duration) => {
-  //     return Object.assign(data, { duration });
-  //   })
-  //   .then((data) => {
-  //     // console.log(data.duration);
-  //     return getTags(data);
-  //   });
+  return Promise.allSettled(promises);
+};
+
+const checkMp3 = (filesArray) => {
+  let promises = [];
+  filesArray.forEach((file) => {
+    promises.push(
+      new Promise((resolve, reject) => {
+        FileType.fromFile(file)
+          .then((data) => {
+            if (data && data.ext === "mp3") {
+              resolve(file);
+            } else {
+              console.log(`ERROR in file ${file}`);
+              reject(`ERROR!! ${file} is not a MP3 file`);
+            }
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      })
+    );
+  });
+  return Promise.allSettled(promises);
 };
 
 const convertSong = (filesArray) => {
-  // let musicFiles = [];
   let promises = [];
   filesArray.forEach((file) => {
-    // console.log(file);
     promises.push(
       new Promise((resolve, reject) => {
         fs.readFile(file, (err, data) => {
@@ -174,26 +174,7 @@ const convertSong = (filesArray) => {
       })
     );
   });
-  // const songPromise = new Promise((resolve, reject) => {
-  //   filesArray.forEach((file) => {
-  //     fs.readFile(file, (err, data) => {
-  //       if (err) {
-  //         console.log("Error convertSong");
-  //         reject(err);
-  //       }
-  //       musicFiles.push({
-  //         music: dataurl.convert({ data, mimetype: "audio/mp3" }),
-  //         filePath: file,
-  //       });
-  // resolve({
-  //   music: dataurl.convert({ data, mimetype: "audio/mp3" }),
-  //   filePath: filePath,
-  // });
-  // });
-  // });
-  // resolve(musicFiles);
-  // });
-  return Promise.all(promises);
+  return Promise.allSettled(promises);
 };
 
 const getAllFiles = (filePath) => {
@@ -202,10 +183,8 @@ const getAllFiles = (filePath) => {
     fs.readdir(filePath, (err, files) => {
       if (err) reject(err);
       else {
-        // console.log("Filenames with the .txt extension:");
         files.forEach((file) => {
           if (path.extname(file) === ".mp3") {
-            // console.log(file);
             filesArray.push(filePath + "/" + file);
           }
         });
@@ -225,23 +204,48 @@ function openFolderDialog(event) {
     .then((result) => {
       console.log(result["filePaths"][0]);
       return getAllFiles(result["filePaths"][0]);
-      // return convertSong(result["filePaths"][0]);
     })
     .then((filesArray) => {
-      console.log(filesArray);
+      return checkMp3(filesArray);
+    })
+    .then((data) => {
+      let filesArray = [];
+      data.forEach((result) => {
+        if (result.status === "fulfilled") {
+          filesArray.push(result.value);
+        }
+      });
+      return filesArray;
+    })
+    .then((filesArray) => {
       return convertSong(filesArray);
     })
     .then((data) => {
+      let filesArray = [];
+      data.forEach((result) => {
+        if (result.status === "fulfilled") {
+          filesArray.push(result.value);
+        }
+      });
+      return filesArray;
+    })
+    .then((data) => {
       console.log("Crossed Convert song");
-      // console.log(data[1].filePath);
-      // console.log(data[0].filePath);
       return createSongObject(data);
       // event.sender.send("modal-file-results", data);
       // ipcMain.send("modal-file-results", data);
     })
     .then((data) => {
-      // console.log(data[0].title);
-      // console.log(data[1].title);
+      let filesArray = [];
+      data.forEach((result) => {
+        if (result.status === "fulfilled") {
+          console.log(result.value.filePath);
+          filesArray.push(result.value);
+        }
+      });
+      return filesArray;
+    })
+    .then((data) => {
       console.log("done everything");
       event.sender.send("modal-file-results", data);
     })
