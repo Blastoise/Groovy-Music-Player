7f7c9b7f642e333ef34ab4ed6948e1243170defa
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-06 09:42:09 +0530

    Removed React-Router-DOM snippet

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-
-// import {
-//   BrowserRouter,
-//   HashRouter,
-//   Link,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
