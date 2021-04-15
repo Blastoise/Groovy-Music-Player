46eccecb10bbfc9b9d9283f3a231aece11093166
Blastoise <ashutoshkumarranchi1@gmail.com>
2021-04-16 00:07:19 +0530

    Added code for implementing repeat and shuffle in Music Component

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -27,36 +27,50 @@ const Music = (props) => {
             </div>
           </div>
           <div className="music-controls">
-            <img
-              src="./backward.svg"
-              alt=""
-              className="prev-button"
-              onClick={props.prevSong}
-            />
-            <img
-              src={props.play === true ? "./pause.svg" : "./play.svg"}
-              alt=""
-              className="play-button"
-              onClick={props.playPauseHandler}
-            />
-            <img
-              src="./forward.svg"
-              alt=""
-              className="next-button"
-              onClick={props.nextSong}
-            />
-            <img
-              src="./shuffle.svg"
-              alt=""
-              className="shuffle-button"
-              onClick={props.handleShuffle}
-            />
-            <img
-              src="./repeat.svg"
-              alt=""
-              onClick={props.repeatSong}
-              className="repeat-button"
-            />
+            <div>
+              <img
+                src="./backward.svg"
+                alt=""
+                className="prev-button"
+                onClick={props.prevSong}
+              />
+            </div>
+            <div>
+              <img
+                src={props.play === true ? "./pause.svg" : "./play.svg"}
+                alt=""
+                className="play-button"
+                onClick={props.playPauseHandler}
+              />
+            </div>
+            <div>
+              <img
+                src="./forward.svg"
+                alt=""
+                className="next-button"
+                onClick={props.nextSong}
+              />
+            </div>
+            <div className={props.shuffle ? "shuffle-on" : "shuffle-off"}>
+              <img
+                src="./shuffle.svg"
+                alt=""
+                className="shuffle-button"
+                onClick={props.handleShuffle}
+              />
+            </div>
+            <div className={props.loop === 2 ? "loop-on" : "loop-off"}>
+              <img
+                src={
+                  props.loop === 1
+                    ? "./repeat_one_48px.svg"
+                    : "./repeat_48px.svg"
+                }
+                alt=""
+                onClick={props.repeatSong}
+                className="repeat-button"
+              />
+            </div>
           </div>
         </div>
       </div>
